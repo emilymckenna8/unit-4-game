31bc01540751c353bd23bd4a9b9cadf9b05e20ba
Emily McKenna <emilymckenna08@gmail.com>
2018-05-11 08:38:02 -0400

    consolidated vars

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,42 @@
-//sets initial variables
 
+document.ready(function(){
+//sets initial variables    
 var wins = 0;
 var losses= 0;
-var firstCrystal="" ;
-var secondCrystal= "";
-var thirdCrystal = "";
-var fourthCrystal = "";
+var firstCrystal= Math.floor(Math.random()*11+1) ;
+var secondCrystal= Math.floor(Math.random()*11+1);
+var thirdCrystal = Math.floor(Math.random()*11+1);
+var fourthCrystal = Math.floor(Math.random()*11+1);
 var userTotal = "";
 
-//user clicks to start the game
 
-
-function firstFunction() {
 //picks a random number for the user's goal
 var randomNumber = Math.floor(Math.random()*101+19);
 //prints randomNumber to check
 console.log(randomNumber);
 //append randomNumber to html
-$("#random-number").html("<p>" + randomNumber + "</p>")
+$("#random-number").html("<p>" + randomNumber + "</p>");
 
+});
 //assigns random values to each crystal
 
 function numOne() {
-    var firstCrystal = Math.floor(Math.random()*11+1)
     console.log(firstCrystal);
 }
 
 function numTwo (){
-    var secondCrystal = Math.floor(Math.random()*11+1);
+    
     console.log(secondCrystal);
 
 }
 
 function numThree() {
-    var thirdCrystal = Math.floor(Math.random()*11+1);
+    
     console.log(thirdCrystal);
 }
 
 function numFour() {
-    var fourthCrystal = Math.floor(Math.random()*11+1)
+    
     console.log(fourthCrystal);
 }
 
@@ -46,7 +44,7 @@ numOne();
 numTwo();
 numThree();
 numFour();
-};
+
 //page loads, run first function
 firstFunction();
 
